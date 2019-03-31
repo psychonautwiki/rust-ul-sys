7e8e764f56c24f6e3c30b005df57aa8997c278e3
Kenan Sulayman <kenan@sly.mn>
2019-03-31 23:03:27 +0200

    Update to 1.0.1

--- build.rs
+++ build.rs
@@ -1,9 +1,30 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     println!("cargo:rustc-flags=-L /usr/local/lib/");
     println!("cargo:rustc-link-search=/usr/local/lib/");
     println!("cargo:rustc-link-lib=Ultralight");
     println!("cargo:rustc-link-lib=WebCore");
     println!("cargo:rustc-link-lib=AppCore");
-}
 
-// bindgen --use-core --impl-debug --impl-partialeq --generate-inline-functions --dump-preprocessed-input --conservative-inline-namespaces --whitelist-function "^UL.*|JS.*|ul.*|WK.*" --whitelist-var "^UL.*|JS.*|ul.*|WK.*" --whitelist-type "^UL.*|JS.*|ul.*|WK.*"
+//    let bindings = bindgen::Builder::default()
+//        .header("wrapper.h")
+//        .impl_debug(true)
+//        .impl_partialeq(true)
+//        .generate_comments(true)
+//        .generate_inline_functions(true)
+//        .whitelist_var("^UL.*|JS.*|ul.*|WK.*")
+//        .whitelist_type("^UL.*|JS.*|ul.*|WK.*")
+//        .whitelist_function("^UL.*|JS.*|ul.*|WK.*")
+//        .generate()
+//        .expect("Unable to generate bindings");
+//
+//    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+//
+//    bindings
+//        .write_to_file(out_path.join("lib.rs"))
+//        .expect("Couldn't write bindings!");
+}
